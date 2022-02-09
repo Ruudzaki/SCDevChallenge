bf3d15a3765c5abd9b30bc6380bac7bcbaeee90f
Ruudzaki <56171632+Ruudzaki@users.noreply.github.com>
2022-02-09 18:04:09 +0200

    add convertors DT <--> TS

---
+++ SC.DevChallenge.Api.Test/UtillsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using SC.DevChallenge.Api.Application;
+
+namespace SC.DevChallenge.Api.Test
+{
+    [TestFixture]
+    public class UtillsTest
+    {
+        [TestCase(0, ExpectedResult = "2018-01-01 00:00:00")]
+        [TestCase(1, ExpectedResult = "2018-01-01 02:46:40")]
+        [TestCase(2, ExpectedResult = "2018-01-01 05:33:20")]
+        public DateTime TimeSlotToDateTime_TimeSlot_FirstDateTime(int timeSlot)
+        {
+            var utills = new Utills();
+            return utills.TimeSlotToDateTime(timeSlot);
+        }
+
+        [TestCase("2018-01-01 00:00:00", ExpectedResult = 0)]
+        [TestCase("2018-01-01 01:46:40", ExpectedResult = 0)]
+        [TestCase("2018-01-01 02:46:40", ExpectedResult = 1)]
+        [TestCase("2018-01-01 03:46:40", ExpectedResult = 1)]
+        [TestCase("2018-01-01 05:46:40", ExpectedResult = 2)]
+        public int DateTimeToTimeSlot_DateTimeInRange_TimeSlot(DateTime dateTime)
+        {
+            var utills = new Utills();
+            return utills.DateTimeToTimeSlot(dateTime);
+        }
+    }
+}
---
+++ SC.DevChallenge.Api/Application/Utills.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SC.DevChallenge.Api.Application
+{
+    public class Utills
+    {
+        public Utills(DateTime initDate, int timeSlotRange)
+        {
+            InitDateTime = initDate;
+            TimeSlotRange = timeSlotRange;
+        }
+
+        public int TimeSlotRange { get; }
+        public DateTime InitDateTime { get; set; }
+
+        public Utills() : this (new DateTime(2018, 1, 1), 10000) { }
+
+        public int DateTimeToTimeSlot(DateTime dateTime)
+        {
+            int timeSlot = (int) (dateTime.Subtract(InitDateTime).TotalSeconds / 10000);
+
+            return timeSlot;
+        }
+
+        public DateTime TimeSlotToDateTime(int timeSlot)
+        {
+            return InitDateTime.AddSeconds(10000 * timeSlot);
+        }
+    }
+}
